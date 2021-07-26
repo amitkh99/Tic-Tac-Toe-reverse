94e8b90a65cc368f17c625417b455050f5213aac
Amit Khalon <amitkahlon99@mta.ac.il>
2021-07-26 21:50:51 +0300

    Add project files.

---
+++ UI/ButtonTableCell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    internal class ButtonTableCell : Button
+    {
+        private readonly Point r_CellId;
+
+        internal ButtonTableCell(int i_Size, int i_X, int i_Y)
+        {
+            r_CellId.X = i_X;
+            r_CellId.Y = i_Y;
+            this.Width = i_Size;
+            this.Height = i_Size;
+            this.Enabled = true;
+        }
+
+        internal Point CellId { get => r_CellId; }
+    }
+}
---
+++ UI/GameSettingsForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class GameSettingsForm : Form
+    {
+        public GameSettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxPlayer2_CheckedChanged(object sender, EventArgs e)
+        {
+            if ((sender as CheckBox).Checked == true)
+            {
+                textBoxPlayer2.Enabled = true;
+                textBoxPlayer2.Text = string.Empty;
+            }
+            else
+            {
+                textBoxPlayer2.Enabled = false;
+                textBoxPlayer2.Text = "[Computer]";
+            }
+        }
+
+        // Game table must be a square matrix - this method change cols value to be same as rows
+        private void numericUpDownRows_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDownCols.Value = (sender as NumericUpDown).Value;
+        }
+
+        // Game table must be a square matrix - this method change rows value to be same as cols
+        private void numericUpDownCols_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDownRows.Value = (sender as NumericUpDown).Value;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (textBoxPlayer1.Text.Length == 0)
+            {
+                MessageBox.Show("Error - Player 1 name isn't valid.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxPlayer2.Text.Length == 0)
+            {
+                MessageBox.Show("Error - Player 2 name isn't valid.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                GameTableForm gameForm = new GameTableForm((int)numericUpDownRows.Value, textBoxPlayer1.Text, !checkBoxPlayer2.Checked, textBoxPlayer2.Text);
+                this.Visible = false;
+                gameForm.ShowDialog();
+                this.Close();
+            }
+        }
+    }
+}
